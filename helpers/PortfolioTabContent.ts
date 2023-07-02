a3679f86d6c771e1c6cd674ac478f5544a4bccd2
ZeiZel <valera2003lvov@yandex.ru>
2023-07-02 10:31:02 +0300

    portfolio img

--- helpers/PortfolioTabContent.ts
+++ helpers/PortfolioTabContent.ts
@@ -7,68 +7,99 @@ export interface IPortfolioTabItem {
 
 export const hiddenPortfolioItemsArray: IPortfolioTabItem[] = [
 	{
-		id: 7,
-		img: '/img/portfolio-1.png',
-		link: '#',
-		title: 'Интернет магазин ООО «Волго-Вятский Монетный Двор»',
+		id: 5,
+		img: '/img/portfolio-5.png',
+		link: 'https://github.com/ZeiZel/marvel-app',
+		title: 'Платформа «Marvel»',
 	},
-	{ id: 8, img: '/img/portfolio-2.png', link: '#', title: 'Лендинг «Гитары»' },
-	{ id: 9, img: '/img/portfolio-3.png', link: '#', title: 'Лендинг «LoftHouse»' },
-	{ id: 10, img: '/img/portfolio-1.png', link: '#', title: 'Обучающая платформа «Бризли»' },
 ];
 
 export const portfolioItems1: IPortfolioTabItem[] = [
-	{ id: 2, img: '/img/portfolio-2.png', link: '#', title: 'Лендинг «Гитары»' },
 	{
 		id: 1,
 		img: '/img/portfolio-1.png',
-		link: '#',
+		link: 'https://github.com/ZeiZel/vvmd-e-commerce-app',
 		title: 'Интернет магазин ООО «Волго-Вятский Монетный Двор»',
 	},
-	{ id: 3, img: '/img/portfolio-3.png', link: '#', title: 'Лендинг «LoftHouse»' },
-	{ id: 5, img: '/img/portfolio-2.png', link: '#', title: 'Интернет магазин «Цифровой»' },
-	{ id: 4, img: '/img/portfolio-1.png', link: '#', title: 'Обучающая платформа «Бризли»' },
-	{ id: 6, img: '/img/portfolio-3.png', link: '#', title: 'Сайт грузоперевозок «Сервис»' },
+	{
+		id: 2,
+		img: '/img/portfolio-4.png',
+		link: 'https://github.com/ZeiZel/first-project__guitar',
+		title: 'Лендинг «Гитары»',
+	},
+	{
+		id: 3,
+		img: '/img/portfolio-3.png',
+		link: 'https://github.com/ZeiZel/loft-tut',
+		title: 'Лендинг «LoftHouse»',
+	},
+	{
+		id: 4,
+		img: '/img/portfolio-2.png',
+		link: 'https://github.com/ZeiZel/next-app-courses',
+		title: 'Платформа курсов «OwlTop»',
+	},
 ];
 
 export const portfolioItems2: IPortfolioTabItem[] = [
 	{
 		id: 1,
 		img: '/img/portfolio-1.png',
-		link: '#',
+		link: 'https://github.com/ZeiZel/vvmd-e-commerce-app',
 		title: 'Интернет магазин ООО «Волго-Вятский Монетный Двор»',
 	},
-	{ id: 5, img: '/img/portfolio-2.png', link: '#', title: 'Интернет магазин «Цифровой»' },
-	{ id: 2, img: '/img/portfolio-2.png', link: '#', title: 'Лендинг «Гитары»' },
-	{ id: 3, img: '/img/portfolio-3.png', link: '#', title: 'Лендинг «LoftHouse»' },
-	{ id: 4, img: '/img/portfolio-1.png', link: '#', title: 'Обучающая платформа «Бризли»' },
-	{ id: 6, img: '/img/portfolio-3.png', link: '#', title: 'Сайт грузоперевозок «Сервис»' },
 ];
 
 export const portfolioItems3: IPortfolioTabItem[] = [
 	{
 		id: 1,
 		img: '/img/portfolio-1.png',
-		link: '#',
+		link: 'https://github.com/ZeiZel/vvmd-e-commerce-app',
 		title: 'Интернет магазин ООО «Волго-Вятский Монетный Двор»',
 	},
-	{ id: 6, img: '/img/portfolio-3.png', link: '#', title: 'Сайт грузоперевозок «Сервис»' },
-	{ id: 2, img: '/img/portfolio-2.png', link: '#', title: 'Лендинг «Гитары»' },
-	{ id: 3, img: '/img/portfolio-3.png', link: '#', title: 'Лендинг «LoftHouse»' },
-	{ id: 4, img: '/img/portfolio-1.png', link: '#', title: 'Обучающая платформа «Бризли»' },
-	{ id: 5, img: '/img/portfolio-2.png', link: '#', title: 'Интернет магазин «Цифровой»' },
+	{
+		id: 2,
+		img: '/img/portfolio-4.png',
+		link: 'https://github.com/ZeiZel/first-project__guitar',
+		title: 'Лендинг «Гитары»',
+	},
+	{
+		id: 3,
+		img: '/img/portfolio-3.png',
+		link: 'https://github.com/ZeiZel/loft-tut',
+		title: 'Лендинг «LoftHouse»',
+	},
+	{
+		id: 4,
+		img: '/img/portfolio-2.png',
+		link: 'https://github.com/ZeiZel/next-app-courses',
+		title: 'Платформа курсов «OwlTop»',
+	},
 ];
 
 export const portfolioItems4: IPortfolioTabItem[] = [
 	{
 		id: 1,
 		img: '/img/portfolio-1.png',
-		link: '#',
+		link: 'https://github.com/ZeiZel/vvmd-e-commerce-app',
 		title: 'Интернет магазин ООО «Волго-Вятский Монетный Двор»',
 	},
-	{ id: 2, img: '/img/portfolio-2.png', link: '#', title: 'Лендинг «Гитары»' },
-	{ id: 3, img: '/img/portfolio-3.png', link: '#', title: 'Лендинг «LoftHouse»' },
-	{ id: 4, img: '/img/portfolio-1.png', link: '#', title: 'Обучающая платформа «Бризли»' },
-	{ id: 5, img: '/img/portfolio-2.png', link: '#', title: 'Интернет магазин «Цифровой»' },
-	{ id: 6, img: '/img/portfolio-3.png', link: '#', title: 'Сайт грузоперевозок «Сервис»' },
+	{
+		id: 2,
+		img: '/img/portfolio-4.png',
+		link: 'https://github.com/ZeiZel/first-project__guitar',
+		title: 'Лендинг «Гитары»',
+	},
+	{
+		id: 3,
+		img: '/img/portfolio-3.png',
+		link: 'https://github.com/ZeiZel/loft-tut',
+		title: 'Лендинг «LoftHouse»',
+	},
+	{
+		id: 4,
+		img: '/img/portfolio-2.png',
+		link: 'https://github.com/ZeiZel/next-app-courses',
+		title: 'Платформа курсов «OwlTop»',
+	},
 ];
